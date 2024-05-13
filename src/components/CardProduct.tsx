c5cc8284e3487708323df51946fbca42a8cf1f53
Arya Priema <99aryaprima@gmail.com>
2024-05-13 16:30:36 +0700

    styles: CardProduct - hover card

--- src/components/CardProduct.tsx
+++ src/components/CardProduct.tsx
@@ -8,10 +8,11 @@ import {
 	CardTitle,
 } from '@/components/ui/card';
 import type { Product } from '@/app/page';
+import { Button } from './ui/button';
 
 const CardProduct = ({ product }: { product: Product }) => {
 	return (
-		<Card className='p-8 break-inside-avoid'>
+		<Card className='p-8 break-inside-avoid group'>
 			<CardContent className='p-0 flex flex-col space-y-6'>
 				<div className='bg-zinc-100 relative p-10 grid place-items-center rounded-lg overflow-hidden'>
 					<img
@@ -21,6 +22,13 @@ const CardProduct = ({ product }: { product: Product }) => {
 						height={350}
 						className='object-cover w-[200px] h-[200px] rounded-lg mix-blend-multiply'
 					/>
+
+					<div className='hidden group-hover:block transition duration-200 absolute bottom-6 left-6 right-6'>
+						{/* TODO: add to cart */}
+						<Button className='w-full rounded-none bg-zinc-900 hover:bg-zinc-900/90'>
+							ADD TO CART
+						</Button>
+					</div>
 				</div>
 				<div className='flex flex-col space-y-1'>
 					<CardDescription className='text-zinc-800'>
